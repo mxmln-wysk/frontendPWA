1e6b3bb12eea98206bae93935eb094ff7db21239
mxmln-wysk <maximilian.wysk@gmx.net>
2021-11-13 20:21:49 +0100

    deleted unused theme code part II

--- packages/mars-theme/src/index.js
+++ packages/mars-theme/src/index.js
@@ -6,17 +6,9 @@ import link from "@frontity/html2react/processors/link";
 const marsTheme = {
   name: "@frontity/mars-theme",
   roots: {
-    /**
-     * In Frontity, any package can add React components to the site.
-     * We use roots for that, scoped to the `theme` namespace.
-     */
     theme: Theme,
   },
   state: {
-    /**
-     * State is where the packages store their default settings and other
-     * relevant state. It is scoped to the `theme` namespace.
-     */
     theme: {
       autoPrefetch: "in-view",
       menu: [],
@@ -28,27 +20,8 @@ const marsTheme = {
     },
   },
 
-  /**
-   * Actions are functions that modify the state or deal with other parts of
-   * Frontity like libraries.
-   */
-  actions: {
-    theme: {
-      toggleMobileMenu: ({ state }) => {
-        state.theme.isMobileMenuOpen = !state.theme.isMobileMenuOpen;
-      },
-      closeMobileMenu: ({ state }) => {
-        state.theme.isMobileMenuOpen = false;
-      },
-    },
-  },
   libraries: {
     html2react: {
-      /**
-       * Add a processor to `html2react` so it processes the `<img>` tags
-       * and internal link inside the content HTML.
-       * You can add your own processors too.
-       */
       processors: [image, iframe, link],
     },
   },
