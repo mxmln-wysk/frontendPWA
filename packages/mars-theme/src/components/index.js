a541a3fc37484d862fbe61253d688af75b424034
mxmln-wysk <maximilian.wysk@gmx.net>
2021-11-21 21:29:13 +0100

    antother try

--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -19,7 +19,7 @@ const Theme = ({ state }) => {
       <Head>
         <meta name="description" content={state.frontity.description} />
         <html lang="en" />
-        <link rel="manifest" href={'https://dormapfad.de/Media/manifest.json'} />
+        <link rel="manifest" href={`${state.source.api}/wp/v2/web-app-manifest`} />
       </Head>
 
       <Global styles={globalStyles} />
